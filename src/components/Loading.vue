cfa07f7c8cc668a1c2ee7e4af7504ca8b9f3e47c
Diogo Eleutero <desdiogo10@yahoo.com>
2025-05-13 22:49:11 -0300

    Add Loading component with Skeleton placeholders
    Introduced a new `Loading.vue` component that uses the `Skeleton` elements from the UI library. This component will serve as a visual placeholder during data loading states, improving user experience.

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Skeleton } from '@/components/ui/skeleton'
+</script>
+
+<template>
+  <div class="h-full flex flex-col space-y-3 items-center justify-center">
+    <Skeleton class="h-[125px] w-[250px] rounded-xl" />
+    <div class="space-y-2">
+      <Skeleton class="h-4 w-[250px]" />
+      <Skeleton class="h-4 w-[200px]" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
