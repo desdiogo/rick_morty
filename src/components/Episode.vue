b9e43bf12e9b7b9bd214889dd861f5e6302ac265
Diogo Eleutero <desdiogo10@yahoo.com>
2025-05-13 22:46:51 -0300

    Add Episode component and related config files
    Introduce a new `Episode.vue` component to display episode details with reusable UI components. Add `components.json` for project configuration and a utility function `cn` in `utils.ts` for handling class merging.

---
+++ src/components/Episode.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+} from '@/components/ui/card'
+import Text from '@/components/Text.vue'
+import type { Episode } from '@/@types'
+
+defineProps<Episode>()
+</script>
+
+<template>
+  <Card class="w-[300px] cursor-pointer">
+    <CardContent>
+      <Text label="episode" :text="episode" />
+      <Text label="air date" :text="air_date" />
+    </CardContent>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
