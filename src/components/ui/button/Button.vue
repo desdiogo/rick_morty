ee69304a26d4a32cdeb166823194f0490c4ae3fe
Diogo Eleutero <desdiogo10@yahoo.com>
2025-05-13 22:43:29 -0300

    Add UI components for Button and Card with subcomponents
    Created a reusable Button component and a modular Card component with subcomponents: CardHeader, CardFooter, CardTitle, CardDescription, CardContent, and CardAction. These components utilize utility classes and props for flexibility and customization.

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { Primitive, type PrimitiveProps } from 'reka-ui'
+import { type ButtonVariants, buttonVariants } from '.'
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+}
+
+const {as = 'button', class: className} = defineProps<Props>()
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), className)"
+  >
+    <slot />
+  </Primitive>
+</template>
