df197334f4abf73ab04803e8b94e9f7db7cce987
Diogo Eleutero <desdiogo10@yahoo.com>
2025-05-13 22:47:02 -0300

    Add Episode component and related config files
    Introduce a new `Episode.vue` component to display episode details with reusable UI components. Add `components.json` for project configuration and a utility function `cn` in `utils.ts` for handling class merging.Add Episode and Text components for card display
    
    Introduced `Text` component to present labeled text pairs and `Episode` component to display episode details within a card structure. These additions enhance modularity and reusability for UI elements.

---
+++ src/components/Text.vue
@@ -0,0 +1,14 @@
+<!-- components/LabelText.vue -->
+<script setup lang="ts">
+defineProps<{
+  label: string
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <span class="font-bold capitalize">{{ label }}:</span>
+    <span class="capitalize">{{ text }}</span>
+  </div>
+</template>
